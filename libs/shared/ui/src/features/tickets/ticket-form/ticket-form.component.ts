39407be28a38736a6322425d847bb2219d8f052b
Artem Norenko <norenko.artem@gmail.com>
2026-03-07 22:29:55 +0200

    25 updating ticket related items

--- libs/shared/ui/src/features/tickets/ticket-form/ticket-form.component.ts
+++ libs/shared/ui/src/features/tickets/ticket-form/ticket-form.component.ts
@@ -4,33 +4,52 @@ import {
   Output,
   inject,
 } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { EventEmitter } from '@angular/core';
 import { ButtonComponent } from '../../../lib/button/button.component';
-import { TicketFormType } from './ticket-form.type';
+import {
+  TicketForm,
+  TICKET_PRIORITIES as priorities,
+  TICKET_STATUSES as statuses,
+} from './ticket-form.type';
+import { FormFieldComponent } from '../../../lib/form-field/form-field.component';
 
 @Component({
   selector: 'ui-ticket-form',
-  imports: [ButtonComponent, ReactiveFormsModule],
+  imports: [
+    FormFieldComponent,
+    ButtonComponent,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './ticket-form.component.html',
   styleUrl: './ticket-form.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TicketFormComponent {
   protected submitButtonLabel = 'Submit';
-  protected formTitleLabel = 'Title';
-  private fb = inject(FormBuilder);
-
+  private formBuilder = inject(FormBuilder);
+  readonly ticketPriorities = priorities;
+  readonly ticketStatuses = statuses;
   @Output()
-  save = new EventEmitter<TicketFormType>();
+  save = new EventEmitter<TicketForm>();
 
-  readonly form = this.fb.group({
-    title: [''],
-    description: ['Ticket description'],
-    priority: ['low'],
+  readonly ticketForm = this.formBuilder.group({
+    title: ['Help Desk Request form'],
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    email: ['', Validators.email],
+    details: ['', Validators.required],
+    priority: ['', Validators.required],
+    status: ['new', Validators.required],
   });
 
   submit() {
-    this.save.emit(this.form.getRawValue() as TicketFormType);
+    this.save.emit(this.ticketForm.getRawValue() as TicketForm);
   }
 }
--- libs/shared/ui/src/features/tickets/ticket-form/ticket-form.type.ts
+++ libs/shared/ui/src/features/tickets/ticket-form/ticket-form.type.ts
@@ -1,6 +1,35 @@
-export type TicketFormType = {
-  formType: 'create' | 'edit';
+export const TICKET_PRIORITIES = [
+  { value: 'low', label: 'Low priority' },
+  { value: 'medium', label: 'Medium priority' },
+  { value: 'high', label: 'High priority' },
+  { value: 'critical', label: 'Critical priority' },
+] as const;
+
+export const FORM_TYPES = ['create', 'edit'] as const;
+
+export const TICKET_STATUSES = [
+  { value: 'new', label: 'New' },
+  { value: 'open', label: 'Open' },
+  { value: 'in_progress', label: 'In Progress' },
+  { value: 'resolved', label: 'Resolved' },
+  { value: 'closed', label: 'Closed' },
+] as const;
+
+export type TicketPriorityType = (typeof TICKET_PRIORITIES)[number]['value'];
+export type TicketFormType = (typeof FORM_TYPES)[number];
+export type TicketStatusType = (typeof TICKET_STATUSES)[number]['value'];
+
+export interface TicketForm {
+  formType: TicketFormType;
   title: string;
-  description: string;
-  priority: 'low' | 'medium' | 'high';
-};
+  firstName: string;
+  lastName: string;
+  email: string;
+  details: string;
+  priority: TicketPriorityType;
+  status?: TicketStatusType;
+  createdAt?: Date;
+  startedAt?: Date;
+  resolvedAt?: Date;
+  comments?: string[];
+}
