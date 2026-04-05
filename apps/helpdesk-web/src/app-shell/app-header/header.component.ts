aed89053f26458ae4e76403d35eb08a3642c8489
Artem Norenko <norenko.artem@gmail.com>
2026-04-05 21:56:03 +0300

    added rbac base functionality

--- apps/helpdesk-web/src/app-shell/app-header/header.component.ts
+++ apps/helpdesk-web/src/app-shell/app-header/header.component.ts
@@ -9,26 +9,10 @@ import {
 
 import { ButtonComponent, InputComponent, AuthStateService } from '@shared-ui';
 import { RouterLink } from '@angular/router';
-import { NavItem } from '../../app-nav-menu/nav-item';
-import { AppNavMenuComponent } from '../../app-nav-menu/app-nav-menu.component';
-import { NgIf, NgTemplateOutlet } from '@angular/common';
-
-const navItems: NavItem[] = [
-  { label: 'Dashboard', icon: '📊', link: '/dashboard' },
-  { label: 'Tickets', icon: '🎫', link: '/tickets' },
-  { label: 'Reports', icon: '📈', link: '/reports', requiresAuth: true },
-  { label: 'Settings', icon: '⚙️', link: '/settings', requiresAuth: true },
-];
 
 @Component({
   selector: 'app-header',
-  imports: [
-    ButtonComponent,
-    InputComponent,
-    AppNavMenuComponent,
-    RouterLink,
-    NgTemplateOutlet,
-  ],
+  imports: [ButtonComponent, InputComponent, RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
   changeDetection: ChangeDetectionStrategy.Default,
@@ -40,17 +24,13 @@ export class HeaderComponent {
   constructor() {
     this.authStateService.currentUser$.subscribe((user) => {
       this.isAuthorized = !!user;
-      console.log('Auth state changed, current user:', user);
-      console.log('User is authorized:', this.isAuthorized);
     });
   }
-
-  navItems = navItems;
 
   @Input({ required: true }) title = '';
   @Input() showMenu = false;
 
-  @Output() menuClick = new EventEmitter<void>();
+  @Output() menuButtonClick = new EventEmitter<boolean>();
   @Output() requestAssistance = new EventEmitter<void>();
   @Output() searchInput = new EventEmitter<Event>();
   @Output() loginClick = new EventEmitter<void>();
@@ -59,8 +39,7 @@ export class HeaderComponent {
   protected assistanceButtonLabel = 'Request Assistance';
   protected loginButtonLabel = 'Login';
 
-  toggleMenu(): void {
-    this.showMenu = !this.showMenu;
-    console.log('Menu toggled:', this.showMenu);
+  onMenuClick(): void {
+    this.menuButtonClick.emit(!this.showMenu);
   }
 }
